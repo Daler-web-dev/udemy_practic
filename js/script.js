decac496743886ed3ad0f559d36548bfe532d4c4
Daler <dsarifkulo021@gmail.com>
2021-01-17 16:00:26 +0500

    if '?' switch case

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '')
-
-const personalMovieDb = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+if(1) {
+    console.log('ok!');
+} else {
+    console.log('error');
 }
 
-const a = prompt('один из последних просмотренных фильмов', ''),
-      b = prompt('на сколько вы его оцените?', ''),
-      c = prompt('один из последних просмотренных фильмов', ''),
-      d = prompt('на сколько вы его оцените?', '')
+if(num < 49) {
+    console.log('error');
+} else if  (num > 100) {
+    console.log('much');
+} else {
+    console.log('ok');
+}
 
-personalMovieDb.movies[a] = b  
-personalMovieDb.movies[c] = d
+(num == 50) ? console.log('ok') : console.log('error'); 
 
-console.log(personalMovieDb);
+const num = '51'  
+
+switch (num) { 
+    case '49':
+        console.log('noooo')
+        break
+    case '100':
+        console.log('noooo')
+        break
+    case '50':
+        console.log('yess')
+        break
+    default:
+        console.log('не в этот раз');
+        break
+}
